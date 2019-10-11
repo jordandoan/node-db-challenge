66926fadd34d6dad20c3d5259d7670a1f7153b5c
Jordan <jordandoan@hotmail.com>
2019-10-11 09:44:14 -0700

    POST routes tested. MVP done

--- api/server.js
+++ api/server.js
@@ -10,7 +10,16 @@ server.get("/", (req,res) => {
 
 server.get("/api/projects", (req,res) => {
   db.getProjects()
-    .then(projects => res.status(200).json(projects))
+    .then(projects => {
+      projects.forEach(project => {
+        if (project.completed) {
+          project.completed = true;
+        } else {
+          project.completed = false;
+        }
+      })
+      res.status(200).json(projects)
+    })
 });
 
 server.get("/api/resources", (req,res) => {
@@ -31,5 +40,28 @@ server.get("/api/projects/:id/tasks", (req,res) => {
       res.status(200).json(tasks);
     });
 });
+
+server.post("/api/projects", (req,res) => {
+  const project = req.body;
+  if (!project.completed) {
+    project.completed = false
+  }
+  db.addProject(project)
+    .then(id => res.status(201).json({id: id[0]}))
+    .catch(err => res.json(err));
+
+})
+
+server.post("/api/resources", (req,res) => {
+  db.addResource(req.body)
+    .then(id => res.status(201).json({id: id[0]}))
+})
+
+server.post("/api/projects/:id/tasks", (req,res) => {
+  const task = req.body;
+  task.completed = task.completed || false
+  db.addTask(task)
+    .then(id => res.status(201).json({id: id[0]}))
+})
 
 module.exports = server;
--- projects/projects-model.js
+++ projects/projects-model.js
@@ -3,8 +3,13 @@ const db = require("../data/db-config");
 module.exports = {
   getProjects,
   getResources,
-  getTasks
+  getTasks,
+  addProject,
+  addResource,
+  addTask
 }
+
+
 function getProjects() {
   return db('projects')
 }
@@ -18,5 +23,17 @@ function getTasks(project_id) {
     .where({project_id: project_id})
     .join('projects as p', 'p.id', 't.project_id')
     .select('t.id', 'p.name as project_name', 'p.description as project_description', 't.description as task_description', 't.notes', 't.completed')
+}
+
+function addProject(project) {
+  return db('projects').insert(project)
+}
+
+function addResource(resource) {
+  return db('resources').insert(resource)
+}
+
+function addTask(task) {
+  return db('tasks').insert(task)
 }
 
